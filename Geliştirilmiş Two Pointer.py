7cc383a4d15e25463d79c6c31d6e2dd4f2325406
EmirhanKisa <kisaemirhan57@gmail.com>
2025-10-17 13:52:39 +0300

    Add files via upload

---
+++ Geliştirilmiş Two Pointer.py
@@ -0,0 +1,19 @@
+liste = [2, 7, 11, 15]
+hedef = 9
+
+p1 = 0
+p2 = len(liste) - 1
+
+while p1 < p2:
+
+    toplam = liste[p1] + liste[p2]
+
+    if toplam == hedef:
+        print(f"Birinci index {p1} , İkinci index ise {p2}")
+        break
+
+    elif toplam < hedef:
+        p1 = p1 + 1 # p1'i sağa kaydır
+
+    else: # toplam > hedef
+        p2 = p2 - 1 # p2'yi sola kaydır
