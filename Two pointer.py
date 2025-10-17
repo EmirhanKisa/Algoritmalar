7d62cba6fd404727b35468350967f5f14f9c87c9
EmirhanKisa <kisaemirhan57@gmail.com>
2025-10-17 14:13:33 +0300

    Two pointer.py

--- Two pointer.py
+++ Two pointer.py
@@ -1,5 +1,8 @@
-# 1. index + [Değişicek]
-# 2. index + [Değişicek]
+"""
+Burda aklıma ilk gelen şekilde çözmeye çalıştım. "IndexError: list index out of range" hatası gelmesine rağmen çıktı çıkıyor.
+Ama çok yavaş çalışıyor. Geliştirilmiş veriyonu "Algoritmalar" klasörünün içinde mevcut.
+"""
+
 liste = [2, 7, 11, 15]
 hedef = 9
 
@@ -13,4 +16,3 @@ for i in range(len(liste)): # 4 kere dönücek
 
         else:
             pass
-
