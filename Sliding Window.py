26b7350a1551e7eb2b324b235b39c18cc92f8cbb
EmirhanKisa <kisaemirhan57@gmail.com>
2025-10-23 21:26:20 +0300

    Add files via upload

---
+++ Sliding Window.py
@@ -0,0 +1,20 @@
+liste = [2, 1, 5, 1, 3, 2]
+
+d = 0
+p1 = 0
+p2 = 1
+p3 = 2
+dur = len(liste)
+
+while p3 < dur:
+
+    toplam = liste[p1] + liste[p2] + liste[p3]
+
+    if toplam >= d:
+        d = toplam
+
+    p1 = p1 + 1
+    p2 = p2 + 1
+    p3 = (p3+ 1)
+
+print(d)
